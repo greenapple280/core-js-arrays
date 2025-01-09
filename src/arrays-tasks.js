106dc1df989464a6da9435a8ff7f6e4b6e29fd8f
Tanja Kiseleva <lessy_92@mail.ru>
2025-01-09 22:37:46 +0100

    fix: getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -455,9 +455,12 @@ function getIdentityMatrix(/* n */) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  const newArr = numbers.filter((num) => num % 2 !== 0);
+  const result = newArr.map((element) => numbers.indexOf(element));
+  return result;
 }
+getIndicesOfOddNumbers([1, 2, 3, 4, 5]);
 
 /**
  * Returns the array of RGB Hex strings from the specified array of numbers.
